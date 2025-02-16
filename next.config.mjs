b1f386f4097ed93bee30453948049a12da8d1f8a
XianyoYu <yuxianyo20060819@gmail.com>
2025-02-16 20:52:43 +0800

    debug of deployment -2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-module.exports = {
+const nextConfig = {
     eslint: {
       ignoreDuringBuilds: true,
     },
   };
+  
+  export default nextConfig;
+  
